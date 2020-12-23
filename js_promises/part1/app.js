d765cf7a752832b1e937077b563dec076b300161
megan <msmeganmcmanus@gmail.com>
2020-12-23 08:51:17 -0800

    complete part 1

---
+++ js_promises/part1/app.js
@@ -0,0 +1,22 @@
+const favNum = 4;
+const url = "http://numbersapi.com";
+
+// step 1
+$.getJSON(`${url}/${favNum}?json`).then(data => {
+  console.log(data);
+});
+
+// step 2
+const favNums = [4, 12, 34, 92];
+$.getJSON(`${url}/${numbers}?json`).then(data => {
+  console.log(data);
+});
+
+// step 3
+Promise.all(
+  Array.from({ length: 4 }, () => {
+    return $.getJSON(`${url}/${favNum}?json`);
+  })
+).then(facts => {
+  facts.forEach(data => $("#facts").append(`<p>${data.text}</p>`));
+});
